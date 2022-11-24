80d18d1c4ef541302b70871d09ef417ac144d8b7
Jonghyeon yeo <hybridkernal@gmail.com>
2022-11-24 12:14:52 +0900

    rename - location to git_URL

--- src/main.rs
+++ src/main.rs
@@ -26,28 +26,23 @@ USAGE:
 
     let url = format!("https://duckduckgo.com/?q={}&format=json", query);
 
-    println!("url: {}",url);
-
     let response = surf::get(url.as_str()).await?;
-    let gitURL = response
-        .header("gitURL")
+    let git_URL = response
+        .header("git_URL")
         .map(|xs| xs.as_str().to_owned())
         .unwrap_or_else(Default::default);
-    println!("gitURL: {:?}", gitURL);
-    if gitURL.is_empty() {
+    println!("git_URL: {:?}", git_URL);
+    if git_URL.is_empty() {
         async_std::task::sleep(Duration::from_millis(200)).await;
         std::io::stderr().write_all(b" No results.")?;
         std::process::exit(1);
     }
 
-    let mdurl = format!("{}/blob/master/README.md", gitURL);
-    println!("mdurl: {}",mdurl);
-
     // added for git clone
     // let command = Command::new("git")
     Command::new("git")
             .arg("clone")
-            .arg(gitURL + &".git".to_owned())
+            .arg(git_URL + &".git".to_owned())
             .spawn()
             .expect("failed to execute process");
 
